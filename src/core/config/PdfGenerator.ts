f3f8715b3e85bf9f807a0bcf258cfaf15b374559
Rohan Shirke <rohanshirke25812@gmail.com>
2023-10-30 22:33:01 +0530

    invoice quotation pdf rebuild, to-words library added

--- src/core/config/PdfGenerator.ts
+++ src/core/config/PdfGenerator.ts
@@ -1,8 +1,10 @@
+
 /* eslint-disable prettier/prettier */
 // * This file is used to generate invoice pdf
 
 import { jsPDF } from "jspdf";
 import autoTable from "jspdf-autotable"; // ! depretaited import
+import { ToWords } from 'to-words';
 
 /**
  * Gen Method to print invoice or quotation reciept
@@ -12,6 +14,27 @@ import autoTable from "jspdf-autotable"; // ! depretaited import
  * @param invoiceDetials obj | any
  */
 
+
+const toWords = new ToWords({
+  localeCode: 'en-IN',
+  converterOptions: {
+    currency: true,
+    ignoreDecimal: false,
+    ignoreZeroCurrency: false,
+    doNotAddOnly: false,
+    currencyOptions: { // can be used to override defaults for the selected locale
+      name: 'Rupee',
+      plural: 'Rupees',
+      symbol: '₹',
+      fractionalUnit: {
+        name: 'Paisa',
+        plural: 'Paise',
+        symbol: '',
+      },
+    }
+  }
+});
+
 const Gen = async (
     pdftype: string,
     id: string,
@@ -19,160 +42,378 @@ const Gen = async (
     invoiceDetials: any
 ) => {
     pdfName += "_" + id + (pdftype == "quotation" ? "_quotation" : "_invoice");
-    const columns = [
-        { title: "Id", dataKey: "id" },
-        { title: "Item Name", dataKey: "name" },
-        { title: "Price", dataKey: "price" },
-    ];
+
+    const titleForPdf = pdftype == "quotation" ? "Quotation" : "Invoice";
+
     const doc = new jsPDF({
         orientation: "portrait",
         unit: "in",
         format: "a4",
-    });
-
-    // Quotation Heading
-    doc
-        .setFontSize(16)
+      });
+    
+      // Heading
+      doc
+        .setFontSize(12)
         .setTextColor(0, 0, 0)
-        .setFont('helvetica', "bold")
-        .text(pdftype == "quotation" ? "Quotation" : "Invoice", doc.internal.pageSize.width / 2, 0.5, {
+        .text(titleForPdf, doc.internal.pageSize.width / 2, 0.5, {
             align: "center",
         });
-
-    // Company Logo Img
-    const img = new Image()
-    img.src = invoiceDetials.value.company_details.company_logo;
-    doc.addImage(img, 'JPEG', 0.5, 0.7, 0.7, 0.7);
-
-    // Quotation number
-    doc
-        .setFontSize(10)
-        .setFont('helvetica', "normal")
-        .text((pdftype == "quotation" ? "Quotation" : "Invoice") + "# : " + (pdftype == "quotation" ? invoiceDetials.value.quotation_no : invoiceDetials.value.invoice_no), doc.internal.pageSize.width - 2.8, 0.8);
-
-    // Quotation Creation Date
-    const creationDate = new Date(invoiceDetials.value.date).toDateString();
-    doc
-        .setFontSize(10)
-        .setFont('helvetica', "normal")
-        .text((pdftype == "quotation" ? "Quotation" : "Invoice") + " Date : " + creationDate, doc.internal.pageSize.width - 2.8, 1.0);
-
-    // Quotation Due Date
-    const dueDateText = new Date(invoiceDetials.value.duedate).toDateString();
-    doc
-        .setFontSize(10)
-        .setFont('helvetica', "normal")
-        .text((pdftype == "quotation" ? "Quotation" : "Invoice") + " Due Date : " + dueDateText, doc.internal.pageSize.width - 2.8, 1.2);
-
-    // create a line under heading
-    doc.setLineWidth(0.01).line(0.5, 1.5, 7.75, 1.5);
-
-    // Billing Address
-    doc
-        .setFontSize(9)
-        .setFont('helvetica', "normal")
-        .text(
-            `
-        BIlling Address,
-        ${invoiceDetials.value.customer.first_name} ${invoiceDetials.value.customer.last_name}
-        ${invoiceDetials.value.customer.company_name},
-        ${invoiceDetials.value.customer.address1 ? invoiceDetials.value.customer.address1 : ""}
-        ${invoiceDetials.value.customer.address2 ? invoiceDetials.value.customer.address2 : ""}
-        ${invoiceDetials.value.customer.city ? invoiceDetials.value.customer.city : ""},${invoiceDetials.value.customer.city ? invoiceDetials.value.customer.city : ""}
-        ${invoiceDetials.value.customer.states ? invoiceDetials.value.customer.states : ""},${invoiceDetials.value.customer.country ? invoiceDetials.value.customer.country : ""}
-        `,
-            0.25,
-            1.7,
-            {
-                align: "left",
-                maxWidth: 7.5,
-            }
-        );
-
-    // Billing Address
-    doc
-        .setFontSize(9)
-        .setFont('helvetica', "normal")
-        .text(
-            `
-        Site Address,
-        ${invoiceDetials.value.client.first_name} ${invoiceDetials.value.client.last_name}
-        ${invoiceDetials.value.client.company_name},
-        ${invoiceDetials.value.client.address1 ? invoiceDetials.value.client.address1 : ""}
-        ${invoiceDetials.value.client.address2 ? invoiceDetials.value.client.address2 : ""}
-        ${invoiceDetials.value.client.city ? invoiceDetials.value.client.city : ""},${invoiceDetials.value.client.city ? invoiceDetials.value.client.city : ""}
-        ${invoiceDetials.value.client.states ? invoiceDetials.value.client.states : ""},${invoiceDetials.value.client.country ? invoiceDetials.value.client.country : ""}
-        `,
-            doc.internal.pageSize.width - 3.0,
-            1.7,
-            {
-                align: "left",
-                maxWidth: 7.5,
-            }
-        );
-
-    // Using autoTable plugin
-    const invoice_items = invoiceDetials.value.items.map(
-        ({ name, desc, price, ...rest }) => ({
-            ...rest,
-            price: "Rs " + price.substring(1),
-            name: name + "\n" + desc,
-        })
-    );
-
-    // fixed by autotable
-    console.log(invoice_items);
-
-    //adding the last row [Total] in the table
-    const body = [...invoice_items,
-    [
-        {
-            content: `Total`, colSpan: 2,
-            styles: {
-                fillColor: [124, 95, 240],
-                textColor: [255, 255, 255],
-                halign: 'right',
-                fontStyle: "bold"
-            }
-        },
-        {
-            content: `Rs. ${invoiceDetials.value.total.toFixed(2)}`,
-            styles: {
-                fillColor: [124, 95, 240],
-                textColor: [255, 255, 255],
-                halign: 'left',
-                fontStyle: "bold"
-            }
-        }
-    ],
-    [{
-        content: `\n\nNotes : ${invoiceDetials.value.notes ? invoiceDetials.value.notes : ""}`, colSpan: 3,
-        styles: {
-            fillColor: [255, 255, 255],
-            textColor: [0, 0, 0],
-            halign: 'left',
-            maxWidth: 7.5,
-            fontSize: 9
-        }
-    }]
-    ]
-
-    // main Content in the table
-    autoTable(doc, {
-        columns,
-        startY: 3,
-        body: body,
-        margin: { left: 0.5, top: 1.25 },
-        headStyles: { fillColor: [124, 95, 240] },
-        didDrawCell: function (data) {
-            if (data.column.index === 0) {
-                data.cell.styles.halign = 'center';
-            } else {
-                data.cell.styles.halign = 'right';
-            }
-        }
-    });
     
+      // Company Logo Img
+      const img = new Image()
+      img.src = invoiceDetials.value.company_details.company_logo;
+      doc.addImage(img, 'JPEG', 0.5, 0.7, 0.7, 0.7);
+    
+      // create a line under heading
+      doc.setLineWidth(0.01).line(0.5, 1.5, 7.75, 1.5);
+
+      
+    const creationDate = new Date(invoiceDetials.value.date).toDateString();
+    const validationDate = new Date(invoiceDetials.value.duedate).toDateString();
+
+    const uniqueNumber = (pdftype == "quotation" ? invoiceDetials.value.quotation_no : invoiceDetials.value.invoice_no)
+
+      const Info = [
+        [`${titleForPdf} Date : ${creationDate}`, `#${titleForPdf} No. : ${uniqueNumber}`],
+        [`${titleForPdf} Due Date : ${validationDate}`, `Enquiry No. : UPCOMING`],
+      ];
+    
+      autoTable(doc, {
+        body: Info,
+        startY:1.6,
+        columnStyles: {
+            '0': { cellWidth: 3.63},
+            '1': { cellWidth: "auto" },
+        },
+        margin: { left: 0.5, top: 1.25 },
+        bodyStyles: { halign: "left",fontSize: 9},
+        tableLineColor: [0, 0, 0],
+        didDrawCell: (data) => {
+          const { cell, row, column } = data;
+          doc.rect(cell.x, cell.y, cell.width, cell.height, 'S');
+        },
+      });
+
+      const LocationData = [
+        [
+          `Customer Name : ${invoiceDetials.value.customer.first_name} ${invoiceDetials.value.customer.last_name} \nCompany Name : ${invoiceDetials.value.customer.company_name}\n\nBilling Address : ${invoiceDetials.value.customer.address1 ? invoiceDetials.value.customer.address1 : ""} ${invoiceDetials.value.customer.address2 ? invoiceDetials.value.customer.address2 : ""} ${invoiceDetials.value.customer.city ? invoiceDetials.value.customer.city : ""} - ${invoiceDetials.value.customer.pincode ? invoiceDetials.value.customer.pincode : ""}, ${invoiceDetials.value.customer.states ? invoiceDetials.value.customer.states : ""}, ${invoiceDetials.value.customer.country ? invoiceDetials.value.customer.country : ""}`,
+          `Client Name : ${invoiceDetials.value.client.first_name} ${invoiceDetials.value.client.last_name} \nCompany Name : ${invoiceDetials.value.client.company_name}\n\nSite Address : ${invoiceDetials.value.client.address1 ? invoiceDetials.value.client.address1 : ""} ${invoiceDetials.value.client.address2 ? invoiceDetials.value.client.address2 : ""} ${invoiceDetials.value.client.city ? invoiceDetials.value.client.city : ""} - ${invoiceDetials.value.client.pincode ? invoiceDetials.value.client.pincode : ""}, ${invoiceDetials.value.client.states ? invoiceDetials.value.client.states : ""}, ${invoiceDetials.value.client.country ? invoiceDetials.value.client.country : ""}`
+        ]
+      ];
+
+      autoTable(doc, {
+        body: LocationData,
+        columnStyles: {
+            '0': { cellWidth: 3.63},
+            '1': { cellWidth: "auto" },
+        },
+        margin: { left: 0.5, top: 1.25 },
+        bodyStyles: { halign: "left",fontSize: 9},
+        tableLineColor: [0, 0, 0],
+        didDrawCell: (data) => {
+          const { cell, row, column } = data;
+          doc.rect(cell.x, cell.y, cell.width, cell.height, 'S');
+        },
+      });
+
+      const ChargesItems = [
+
+        [
+            {
+                title: "",
+              },
+              {
+                title: `Site Location : ${invoiceDetials.value.items.site_location} \n${invoiceDetials.value.items.per_day_charge} /- PER DAY`,
+              },
+              {
+                title: ""
+              },
+              {
+                title: `${invoiceDetials.value.items.per_day_charge} x ${invoiceDetials.value.items.number_of_days}` 
+              },
+              {
+                title: ""
+              },
+              {
+                title: `${invoiceDetials.value.items.per_day_charge * invoiceDetials.value.items.number_of_days}`
+              }
+        ],
+        [],
+        [
+            {
+                title: "",
+              },
+              {
+                title: `Other Charges`,
+              }
+        ],
+        [
+            {
+            title: "",
+            },
+            {
+            title: `Accommodation`,
+            },
+            {
+            title: ""
+            },
+            {
+            title: `${invoiceDetials.value.items.accommodation}` 
+            },
+            {
+            title: ""
+            },
+            {
+            title: `${invoiceDetials.value.items.accommodation}`
+            }
+        ],
+        [
+            {
+                title: "",
+              },
+              {
+                title: `Travelling`,
+              },
+              {
+                title: ""
+              },
+              {
+                title: `${invoiceDetials.value.items.travelling}` 
+              },
+              {
+                title: ""
+              },
+              {
+                title: `${invoiceDetials.value.items.travelling}` 
+              }
+        ],
+        [
+            {
+                title: "",
+              },
+              {
+                title: `Training`,
+              },
+              {
+                title: ""
+              },
+              {
+                title: `${invoiceDetials.value.items.training}` 
+              },
+              {
+                title: ""
+              },
+              {
+                title: `${invoiceDetials.value.items.training}` 
+              }
+        ],
+        [
+            {
+                title: "",
+              },
+              {
+                title: `Pickup & Delivery`,
+              },
+              {
+                title: ""
+              },
+              {
+                title: `${invoiceDetials.value.items.pickup}`
+              },
+              {
+                title: ""
+              },
+              {
+                title: `${invoiceDetials.value.items.pickup}`
+              }
+        ],
+        [
+            {
+                title: "",
+              },
+              {
+                title: `Boarding & Lodging`,
+              },
+              {
+                title: ""
+              },
+              {
+                title: `${invoiceDetials.value.items.boarding}`
+              },
+              {
+                title: ""
+              },
+              {
+                title: `${invoiceDetials.value.items.boarding}`
+              }
+        ],
+
+      ]
+
+      
+      let numbersToWords = toWords.convert(invoiceDetials.value.total.toFixed(2), { currency: true });
+
+      const Footer = [
+        [],
+        [
+            {
+                title: "",
+              },
+              {
+                title: "",
+              },
+              {
+                title: ""
+              },
+              {
+                title: ""
+              },
+              {
+                title: "Subtotal"
+              },
+              {
+                title: `${invoiceDetials.value.total}`
+              }
+        ],
+        [
+            {
+                title: "",
+              },
+              {
+                title: "",
+              },
+              {
+                title: ""
+              },
+              {
+                title: ""
+              },
+              {
+                title: "Tax 0%"
+              },
+              {
+                title: ""
+              }
+        ],
+        [
+            {
+                title: "",
+              },
+              {
+                title: "",
+              },
+              {
+                title: ""
+              },
+              {
+                title: ""
+              },
+              {
+                title: "Tax Amount"
+              },
+              {
+                title: `${0}`
+              }
+        ],
+        [
+            {
+                title: "",
+              },
+              {
+                title: "",
+              },
+              {
+                title: ""
+              },
+              {
+                title: ""
+              },
+            {
+                title: `Total Amount`,
+            },
+            {
+                title: `Rs.${invoiceDetials.value.total.toFixed(2)}`,
+            },
+        ],
+      ]
+
+      const Data = [
+        [
+          {
+            title: "Sr.No"
+          },
+          {
+            title: "Scope of Work"
+          },
+          {
+            title: "Quantity"
+          },
+          {
+            title: "Unit Price"
+          },
+          {
+            title: "Discount %"
+          },
+          {
+            title: "Amount"
+          }
+        ],
+        [
+          {
+            title: "",
+          },
+          {
+            title: `${invoiceDetials.value.scope_of_work}`,
+          },
+          {
+            title: ""
+          },
+          {
+            title: `No. of days ${invoiceDetials.value.items.number_of_days}` 
+          },
+          {
+            title: ""
+          },
+          {
+            title: ""
+          }
+        ],
+        ...ChargesItems,
+        ...Footer,
+        [
+          {
+            title: `Amount in words : ${numbersToWords}`,
+            colSpan: 6
+          }
+        ]
+      ];
+
+      
+      autoTable(doc, {
+        body: Data,
+        margin: { left: 0.5, top: 1.25 },
+        bodyStyles: { halign: "left",fontSize: 9},
+        tableLineColor: [0, 0, 0],
+        didDrawCell: (data) => {
+          const { cell, row, column } = data;
+          doc.rect(cell.x, cell.y, cell.width, cell.height, 'S');
+        },
+      });
+
+      const termsAndConditions = [
+        [
+          {
+            title: `Terms & Conditions : \n${invoiceDetials.value.terms_and_conditions}`,
+          }
+        ]
+      ]
+
+      autoTable(doc, {
+        body: termsAndConditions,
+        margin: { left: 0.5, top: 1.25 },
+        bodyStyles: { halign: "left",fontSize: 9},
+        tableLineColor: [0, 0, 0],
+      });
 
     // Creating footer and saving file
     doc
